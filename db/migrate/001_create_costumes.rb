61636cf75a8504781b6673856aeb915cebb5d474
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 00:08:16 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,14 @@
 class CreateCostumes < ActiveRecord::Migration 
   
   def change 
+    create_table :costumes do |t| 
+      t.string :name 
+      t.string :price 
+      t.string :size 
+      t.string :image_url
+      t.datetime :created_at 
+      t.datetime :updated_at 
+    end 
   end 
   
 end 
