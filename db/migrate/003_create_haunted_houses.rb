7bd43e115177a7d96eccc1708a97cb639d28fbe3
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 00:23:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.boolean :family_friendly
       t.datetime :opening_date 
       t.datetime :closing_date 
+      t.string :long_description 
     end 
   end 
   
