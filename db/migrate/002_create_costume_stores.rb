bf007105f230f04a386cd4f0cf40e8b9a934806a
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 00:24:05 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.boolean :still_in_business 
       t.datetime :opening_time 
       t.datetime :closing_time 
+    end 
   end 
   
 end 
